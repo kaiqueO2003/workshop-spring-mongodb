033242c4a609b04dc9a7794d32f8ca63cd387ad2
kaique oliveira <kaique20003@gmail.com>
2024-10-08 13:18:11 -0300

    endpoint para retorna posts de um usuario

--- src/main/java/com/kaiqueoliveira/workshopmongo/resource/UserResource.java
+++ src/main/java/com/kaiqueoliveira/workshopmongo/resource/UserResource.java
@@ -1,5 +1,6 @@
 package com.kaiqueoliveira.workshopmongo.resource;
 
+import com.kaiqueoliveira.workshopmongo.domain.Post;
 import com.kaiqueoliveira.workshopmongo.domain.User;
 import com.kaiqueoliveira.workshopmongo.dto.UserDTO;
 import com.kaiqueoliveira.workshopmongo.service.UserService;
@@ -51,5 +52,10 @@ public class UserResource {
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
 
+    }
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
