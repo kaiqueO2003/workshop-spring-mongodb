b749c8e7e39fb6f394e477abbb7feacb223db654
kaique oliveira <kaique20003@gmail.com>
2024-10-09 10:20:22 -0300

    consulta simples com query methods

--- src/main/java/com/kaiqueoliveira/workshopmongo/resource/PostResource.java
+++ src/main/java/com/kaiqueoliveira/workshopmongo/resource/PostResource.java
@@ -3,6 +3,7 @@ package com.kaiqueoliveira.workshopmongo.resource;
 import com.kaiqueoliveira.workshopmongo.domain.Post;
 import com.kaiqueoliveira.workshopmongo.domain.User;
 import com.kaiqueoliveira.workshopmongo.dto.UserDTO;
+import com.kaiqueoliveira.workshopmongo.resource.util.URL;
 import com.kaiqueoliveira.workshopmongo.service.PostService;
 import com.kaiqueoliveira.workshopmongo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,13 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id){
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTittle(@RequestParam (value = "text", defaultValue = "") String text){
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 
 }
