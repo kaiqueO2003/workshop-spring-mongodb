11fdf54a07bfecdb2a510ff3693bec81f603bdf7
kaique oliveira <kaique20003@gmail.com>
2024-10-09 10:34:41 -0300

    consulta simples com @query

--- src/main/java/com/kaiqueoliveira/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/kaiqueoliveira/workshopmongo/repository/PostRepository.java
@@ -3,12 +3,15 @@ package com.kaiqueoliveira.workshopmongo.repository;
 import com.kaiqueoliveira.workshopmongo.domain.Post;
 import com.kaiqueoliveira.workshopmongo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
